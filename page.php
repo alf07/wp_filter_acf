4660a26bd660876816234c411f97d389835e4444
Roman <roma.alf89@gmail.com>
2022-03-17 13:29:34 +0300

    Create page.php

---
+++ page.php
@@ -0,0 +1,11 @@
+<form action="" method="POST" id="filter">
+		 
+		<div class="polzunok-container-5">
+			<input type="text" name="cena_min" class="polzunok-input-5-left" />
+			<input type="text" name="cena_max" class="polzunok-input-5-right "/>
+			<div class="polzunok-5"></div>
+		</div>
+		
+		<button>Применить фильтр</button><input type="hidden" name="action" value="myfilter">
+	</form>
+	<div id="response"><!-- тут фактически можете вывести посты без фильтрации --></div>
