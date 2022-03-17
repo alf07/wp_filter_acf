82ce80990e8171965883b90e56b91043f234318e
Roman <roma.alf89@gmail.com>
2022-03-17 19:37:32 +0300

    Update filterp.js

--- f/filterp.js
+++ f/filterp.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 $(".polzunok-5").slider({
     min: 0,
     max: 5000,
-    values: [2000, 3000],
+    values: [0, 5000],
     range: true,
     animate: "fast",
     slide : function(event, ui) {    
@@ -45,6 +45,112 @@ $(".polzunok-container-5 input").change(function() {
     }
     if (input_right != where_right) {
         $(".polzunok-5").slider("values", 1, input_right);
+    }
+});
+	});
+})(jQuery);
+
+
+(function($){
+$(document).ready(function(){
+$(".polzunok-1").slider({
+    min: 0,
+    max: 5000,
+    values: [0, 5000],
+    range: true,
+    animate: "fast",
+    slide : function(event, ui) {    
+        $(".polzunok-input-1-left").val(ui.values[ 0 ]);   
+        $(".polzunok-input-1-right").val(ui.values[ 1 ]);  
+    }    
+});
+$(".polzunok-input-1-left").val($(".polzunok-1").slider("values", 0));
+$(".polzunok-input-1-right").val($(".polzunok-1").slider("values", 1));
+$(".polzunok-container-1 input").change(function() {
+    var input_left1 = $(".polzunok-input-1-left").val().replace(/[^0-9]/g, ''),    
+    opt_left1 = $(".polzunok-1").slider("option", "min"),
+    where_right1 = $(".polzunok-1").slider("values", 1),
+    input_right1 = $(".polzunok-input-1-right").val().replace(/[^0-9]/g, ''),    
+    opt_right1 = $(".polzunok-1").slider("option", "max"),
+    where_left1 = $(".polzunok-1").slider("values", 0); 
+    if (input_left1 > where_right1) { 
+        input_left1 = where_right1; 
+    }
+    if (input_left1 < opt_left1) {
+        input_left1 = opt_left1; 
+    }
+    if (input_left1 == "") {
+    input_left1 = 0;    
+    }        
+    if (input_right1 < where_left1) { 
+        input_right1 = where_left1; 
+    }
+    if (input_right1 > opt_right1) {
+        input_right1 = opt_right1; 
+    }
+    if (input_right1 == "") {
+    input_right1 = 0;    
+    }    
+    $(".polzunok-input-1-left").val(input_left1); 
+    $(".polzunok-input-1-right").val(input_right1); 
+    if (input_left1 != where_left1) {
+        $(".polzunok-1").slider("values", 0, input_left1);
+    }
+    if (input_right1 != where_right1) {
+        $(".polzunok-1").slider("values", 1, input_right1);
+    }
+});
+	});
+})(jQuery);
+
+
+(function($){
+$(document).ready(function(){
+$(".polzunok-2").slider({
+    min: 0,
+    max: 5000,
+    values: [0, 5000],
+    range: true,
+    animate: "fast",
+    slide : function(event, ui) {    
+        $(".polzunok-input-2-left").val(ui.values[ 0 ]);   
+        $(".polzunok-input-2-right").val(ui.values[ 1 ]);  
+    }    
+});
+$(".polzunok-input-2-left").val($(".polzunok-2").slider("values", 0));
+$(".polzunok-input-2-right").val($(".polzunok-2").slider("values", 1));
+$(".polzunok-container-2 input").change(function() {
+    var input_left2 = $(".polzunok-input-2-left").val().replace(/[^0-9]/g, ''),    
+    opt_left2 = $(".polzunok-2").slider("option", "min"),
+    where_right2 = $(".polzunok-2").slider("values", 1),
+    input_right2 = $(".polzunok-input-2-right").val().replace(/[^0-9]/g, ''),    
+    opt_right2 = $(".polzunok-2").slider("option", "max"),
+    where_left2 = $(".polzunok-2").slider("values", 0); 
+    if (input_left2 > where_right2) { 
+        input_left2 = where_right2; 
+    }
+    if (input_left2 < opt_left2) {
+        input_left2 = opt_left2; 
+    }
+    if (input_left2 == "") {
+    input_left2 = 0;    
+    }        
+    if (input_right2 < where_left2) { 
+        input_right2 = where_left2; 
+    }
+    if (input_right2 > opt_right2) {
+        input_right2 = opt_right2; 
+    }
+    if (input_right2 == "") {
+    input_right2 = 0;    
+    }    
+    $(".polzunok-input-2-left").val(input_left2); 
+    $(".polzunok-input-2-right").val(input_right2); 
+    if (input_left2 != where_left2) {
+        $(".polzunok-2").slider("values", 0, input_left2);
+    }
+    if (input_right2 != where_right2) {
+        $(".polzunok-2").slider("values", 1, input_right2);
     }
 });
 	});
